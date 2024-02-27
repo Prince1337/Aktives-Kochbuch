e1a031bf93f455575d920691a7c8dc79f3031a6a
Prince1337 <prince.pieritz@gmail.com>
2024-02-27 11:19:47 +0100

    feat: prometheus monitoring

--- src/main/java/prince/aktiveskochbuch/adapter/db/RezeptRepository.java
+++ src/main/java/prince/aktiveskochbuch/adapter/db/RezeptRepository.java
@@ -1,12 +1,14 @@
 package prince.aktiveskochbuch.adapter.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import prince.aktiveskochbuch.domain.models.Rezept;
 
 import java.util.Optional;
 
 @Repository
+@RepositoryRestResource(collectionResourceRel = "rezepte", path = "rezepte")
 public interface RezeptRepository extends JpaRepository<Rezept, Long>{
 
     Optional<Rezept> findByTitel(String title);
